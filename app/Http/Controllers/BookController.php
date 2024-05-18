52bcb0e61c417c9d1f86d4ceea5ccd14fb66e73a
Molnar Mate <molnarmate24@gmail.com>
2024-05-18 15:58:45 +0200

    Könyv törlése

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Models\Borrowing;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
