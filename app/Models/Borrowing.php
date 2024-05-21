85626af7999330031006289fa33f14cf0a3fe2f1
Molnar Mate <molnarmate24@gmail.com>
2024-05-21 12:31:19 +0200

    .

--- app/Models/Borrowing.php
+++ app/Models/Borrowing.php
@@ -26,4 +26,9 @@ class Borrowing extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
 }
